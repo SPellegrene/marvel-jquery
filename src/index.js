5ca3b769b4504bae3cdcba9249840beab2aef970
Sam Pellegrene <pellegrenesam@gmail.com>
2016-11-21 11:06:40 -0600

    fixed text width

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 $(function(){
-  function loadIt(){
+  function loadIt(e){
+    e.preventDefault();
     var linkID = $('#linkID').val();
-    if(linkID == 'undefined' || linkID == '' || linkID == null){
-      alert('Text Only Please')
+    if(typeof linkID == 'undefined' || linkID == '' || linkID == null){
+      alert('Please try again')
     } else {
       console.log(linkID);
       $.ajax({
-        url: "http://gateway.marvel.com:80/v1/public/characters?nameStartsWith="+encodeURIComponent(linkID)+"&apikey=bc8bc29e1ad131498d1f3edfd064b9a1",
+        url: "http://gateway.marvel.com:80/v1/public/characters?nameStartsWith="+ encodeURIComponent(linkID) +"&apikey=bc8bc29e1ad131498d1f3edfd064b9a1",
         crossOrigin: true
       })
-      .then(function(stuff){
-        // console.log('got stuff', stuff);
+      .then(function(response){
         var holder = $('<div class="holders"></div>');
-        $.each(stuff.data.results,function(req, res) {
-          holder.append($('<img/>').attr('src',res.thumbnail.path + '/portrait_uncanny.' + res.thumbnail.extension));
-          holder.append('<br>' + 'Hero: ' + res.name);
-          holder.append('<br>' + 'Copies: ' + res.comics.available);
+        $.each(response.data.results,function(i, character) {
+          holder.append($('<img/>').attr('src',character.thumbnail.path + '/portrait_uncanny.' + character.thumbnail.extension));
+          holder.append('<br>' + 'Hero: ' + character.name);
+          holder.append('<br>' + 'Copies: ' + character.comics.available);
           $('.content').append(holder);
         })
         $('.content').empty().append(holder);
